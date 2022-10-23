791cd5a39752f0eb1bc7a09a872c254e83a08b03
Nikolay Chuprov <nik.adm@gmail.com>
2022-10-23 23:05:14 +0300

    7.16. Отрисуй меня полностью (часть 1)

--- js/data.js
+++ js/data.js
@@ -1,10 +1,17 @@
 import {getRandomIntNumber} from './util.js';
-const createWizard = () => ({
-  id: getRandomIntNumber(1,25),
-  url: `photos/${getRandomIntNumber(1,25)}.jpg`,
-  description: 'Описание фотографии',
-  likes: getRandomIntNumber(15,200),
-  comments: getRandomIntNumber(0,200)
-});
+function createWizard() {
+
+  const p = {
+    id: getRandomIntNumber(1,25),
+    url: `photos/${getRandomIntNumber(1,25)}.jpg`,
+    description: 'Описание фотографии',
+    likes: getRandomIntNumber(15,200),
+    comments: getRandomIntNumber(0,200)
+  };
+
+  return p;
+
+};
 
 const similarWizards = Array.from({length: 25}, createWizard);
+export {similarWizards};
