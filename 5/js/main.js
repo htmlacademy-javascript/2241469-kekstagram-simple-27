314ba55f872c8c0a3cb40159e5be42d8a7e07ab9
Keks <keks@htmlacademy.ru>
2022-10-24 08:47:48 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,12 @@
+//data.js для работы с данными
+import './data.js';
+//view-image.js раздел Просмотр загруженных изображений
+import './view-image.js';
+//util.js утилиты и раздел Дополнительные задания
+import './util.js';
+// upload-image.js раздел Загрузка нового изображения на сайт и заполнение информации о нём
+import './upload-image.js';
+//edit-image.js раздед Редактирование изображения и ограничения, накладываемые на поля
+import './edit-image.js';
+//send-data-to-server.js раздел Отправка данных на сервер
+import './send-data-to-server.js';
---
+++ 5/js/util.js
@@ -0,0 +1,21 @@
+function getRandomIntNumber(startRange, endRange) {
+  if(startRange < 0 || endRange < 0) {
+    return NaN;
+  }
+
+  if(startRange >= endRange) {
+    return NaN;
+  }
+
+  const lower = Math.ceil(startRange);
+  const upper = Math.floor(endRange);
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+
+function checkStringLen(str,maxLen){
+  return str.length <= maxLen;
+}
+
+export {getRandomIntNumber,checkStringLen};
